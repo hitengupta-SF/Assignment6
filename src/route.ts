d7bcdde7d4e9ccc7d6df18a97dbc102bf652e0de
Hiten Gupta <97721485+hitengupta-SF@users.noreply.github.com>
2022-04-08 13:14:26 +0530

    Update route.ts

--- src/route.ts
+++ src/route.ts
@@ -7,7 +7,6 @@ const route = express.Router();
 
 route.get('/users', userController.getAll);
 route.get('/users/:id', userController.getUserById);
-route.post('/add', userController.createUser);
 route.put('/update/:id', userController.updateUser);
 route.delete('/delete/:id', userController.deleteUser);
 export default route;
